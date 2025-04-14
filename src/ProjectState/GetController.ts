09aede0670b0dd0819337a9c4bdc5ee7a37706bf
mtrifonov <mtrifonov.design@gmail.com>
2025-04-14 17:32:03 +0200

    first usable version in place

--- src/ProjectState/GetController.ts
+++ src/ProjectState/GetController.ts
@@ -1,39 +1,31 @@
 import { ProjectDataStructure, PinsAndCurvesProjectController } from "@mtrifonov-design/pinsandcurves-external";
 import * as ext from "@mtrifonov-design/pinsandcurves-external";
 
-function getController(dispatch : any) {
-    //sendMessage("CORE::CORE::CORE", "getController");
+function getController(dispatch : any, worm?: any) {
     const pb = new ProjectDataStructure.ProjectBuilder();
-    pb.addContinuousSignal('signal1', 'signal1.x', [1, 5]);
-    pb.addPin('signal1', 20, 1, 'return easyLinear()');
-    pb.addPin('signal1', 50, 2, 'return easyLinear()');
-    pb.addPin('signal1', 60, 3, 'return easyLinear()');
+    pb.addContinuousSignal('signal1', 'signal 1', [0, 1]);
+    pb.addPin('signal1', 20, 0, 'return easyLinear()');
+    pb.addPin('signal1', 50, 1, 'return easyLinear()');
+    pb.addPin('signal1', 60, 0, 'return easyLinear()');
     pb.setSignalActiveStatus('signal1', true);
-    pb.addContinuousSignal('signal3', 'folder1.signal1.y', [1, 5]);
-    pb.addPin('signal3', 20, 1, 'return easyLinear()');
-    pb.addPin('signal3', 50, 2, 'return easyLinear()');
-    pb.addPin('signal3', 60, 3, 'return easyLinear()');
-    pb.setSignalActiveStatus('signal3', true);
-    pb.addDiscreteSignal('signal2', 'folder1.signal1.x');
-    pb.addPin('signal2', 15, "A");
-    pb.addPin('signal2', 25, "B");
-    pb.addPin('signal2', 35, "C");
-    pb.addPin('signal2', 45, "D");
-    pb.setSignalActiveStatus('signal2', true);
-    pb.addStaticNumberSignal('signal4', 'folder1.signal1.x', [1, 5], 3);
-    pb.addStaticNumberSignal('signal5', 'folder1.signal1.x', [1, 5], 3);
-    pb.addStaticNumberSignal('signal6', 'folder1.signal1.x', [1, 5], 3);
-    pb.addStaticNumberSignal('signal7', 'folder1.signal1.x', [1, 5], 3);
-    pb.addStaticNumberSignal('signal8', 'folder1.signal1.x', [1, 5], 3);
-    pb.addStaticStringSignal('HIDDEN_CODE', "__hidden_code", "function setup() {\n    createCanvas(400, 400);\n    background(220);\n  }")
-    // sendMessage("CORE::CORE::CORE", "getController2");
-    // sendMessage("CORE::CORE::CORE", ext.ProjectDataStructure);
-    // sendMessage("CORE::CORE::CORE", ProjectDataStructure);
-    const controller = PinsAndCurvesProjectController.PinsAndCurvesProjectController.Host(
-        dispatch,
-        pb.getProject(),
-    )
-    // sendMessage("CORE::CORE::CORE", "getController3");
+    pb.addStaticStringSignal('HIDDEN_CODE', "__hidden_code", `
+function setup() {
+    createCanvas(400, 400);
+}
+function draw() {
+    clear();
+    background(220);
+    circle(200, 200, signal("signal 1") * 100);
+}
+`)
+    let controller;
+    if (worm) {
+        controller = PinsAndCurvesProjectController.PinsAndCurvesProjectController
+        .HostFromSerializedWorm(dispatch,worm);
+    } else {
+        controller = PinsAndCurvesProjectController.PinsAndCurvesProjectController
+        .Host(dispatch, pb.getProject());
+    }
     return controller;
 }
 
--- src/ProjectState/index.ts
+++ src/ProjectState/index.ts
@@ -6,27 +6,34 @@ type ProjectNodeEventDispatcher = PinsAndCurvesProjectController.ProjectNodeEven
 class State {
     controller: PinsAndCurvesProjectController.PinsAndCurvesProjectController;
     constructor() {
-        const dispatch = (e: any) => this.emit(e);
+        const dispatch = (e: any) => this.send(e);
         this.controller = getController(dispatch);
     }
+
+    changeController(worm: any) {
+        const dispatch = (e: any) => this.send(e);
+        this.controller = getController(dispatch, worm);
+    }
+
+    send(payload: any) {
+        if (this.receiver_id === "") return;
+        if (payload.type === "hostIsConnected") return;
+        this.addToWorkload(this.subscribers[this.receiver_id], "projectNodeEvent", payload, this.receiver_id);
+    }
+
+    receiver_id: string = "";
     channel : string = "ProjectState";
     subscribers : { [instance_id: string]: any } = {};
-    subscribe(receiver : { instance_id: string, modality: string, resource_id: string }) {
-        // if (!this.subscribers.includes(address)) {
-        //     this.subscribers.push(address)
-        // }
-        this.subscribers[receiver.instance_id] = receiver;
-
-
-        // send current state to address
-        this.addToWorkload(receiver, "subscribeConfirmation", true);
+    subscribe(receiver : { instance_id: string, modality: string, resource_id: string }, subscriber_id: string) {
+        this.subscribers[subscriber_id] = receiver;
+        this.addToWorkload(receiver, "subscribeConfirmation", true, subscriber_id);
     }
     workload : { [thread: string]: any[]} = {};
     addToWorkload(receiver : {
         instance_id: string,
         modality: string,
         resource_id: string
-    }, request: string, messagePayload: any) {
+    }, request: string, messagePayload: any, subscriber_id: string) {
         if (!this.workload["default"]) {
             this.workload["default"] = [];
         }
@@ -38,20 +45,17 @@ class State {
                     channel: "ProjectState",
                     request: request,
                     payload: messagePayload,
+                    subscriber_id,
                 }
             }
         )
     }
     emit(payload: any) {
-        Object.values(this.subscribers).forEach(receiver => {
-            this.addToWorkload(receiver, "projectNodeEvent", payload);
+        Object.keys(this.subscribers).forEach(subscriber_id => {
+            this.addToWorkload(this.subscribers[subscriber_id], "projectNodeEvent", payload, subscriber_id);
         })
     }
-    distribute(source: string, request: string, payload: any) {
-        // const otherSubscribers = this.subscribers.filter(subscriber => subscriber !== source);
-        // otherSubscribers.forEach(address => {
-        //     this.sendTo(address, request, payload);
-        // })
+    distribute(payload: any) {
         this.emit(payload);
         this.controller.receive(payload);
     }
@@ -67,34 +71,50 @@ const state = new State()
 function onCompute(string: string) {
     string = decodeURI(string);
     const unit = JSON.parse(string);
-    log(unit)
-    const { channel, request, payload } = unit.payload;
+    const { channel, request, payload, subscriber_id, SAVE_SESSION, LOAD_SESSION, key, state:s  } = unit.payload;
 
-    // sendMessage("CORE::CORE::CORE", controller);
 
+    if (SAVE_SESSION) {
+        return {
+            persistence: [{
+                type: "worker",
+                receiver: unit.sender,
+                payload: {
+                    key,
+                    SAVE_SESSION,
+                    state: {
+                        worm:state.controller.serializeWorm(),
+                        //subscribers: state.subscribers,
+                    }
+                }
+            }]
+        };
+    }
+
+    if (LOAD_SESSION) {
+        const { worm, subscribers} = s;
+        //state.subscribers = subscribers;
+        state.changeController(worm);
+        return {};
+    }
+
+
+    state.receiver_id = subscriber_id;
     if (channel === "ProjectState") {
         if (request === "subscribe") {
-            state.subscribe(unit.sender);
-            // @ts-ignore
-            return {};
+            state.subscribe(unit.sender, subscriber_id);
+            const workload = state.workload;
+            state.clearWorkload();
+            return workload;
 
         } 
         if (request === "projectNodeEvent") {
-            state.distribute(unit.sender, "projectNodeEvent", payload);
+            state.distribute(payload);
             const workload = state.workload;
             state.clearWorkload();
             return workload;
         }
-
-        // @ts-ignore
-        // sendMessage(source, {
-        //     channel: "responseProject",
-        //     request,
-        //     payload: controller.getProject(),
-        // })
     }
-
-
 }
 
 
